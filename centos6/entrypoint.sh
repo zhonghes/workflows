70389aff3159e8f010e2dba98c0b6710b4a38359
zhonghes <114350562+zhonghes@users.noreply.github.com>
2024-08-22 11:33:29 +0800

    Update entrypoint.sh

--- centos6/entrypoint.sh
+++ centos6/entrypoint.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-# # 复用腾讯云 centos6 yum 源
-# rm -f /etc/yum.repos.d/*.repo
-# curl -fsSL -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos6_base.repo
+# 复用腾讯云 centos6 yum 源
+rm -f /etc/yum.repos.d/*.repo
+curl -fsSL -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos6_base.repo
 
-# # 安装编译环境
-# yum groupinstall -y "Development Tools"
-# yum install -y imake rpm-build pam-devel krb5-devel zlib-devel libXt-devel libX11-devel gtk2-devel perl perl-IPC-Cmd
+# 安装编译环境
+yum groupinstall -y "Development Tools"
+yum install -y imake rpm-build pam-devel krb5-devel zlib-devel libXt-devel libX11-devel gtk2-devel perl perl-IPC-Cmd
 
-# # 拉取 openssh-rpms 仓库
-# cd "$(mktemp -d)" || echo "进入临时目录失败"
-# curl -fsSL -o main.zip https://github.com/boypt/openssh-rpms/archive/refs/heads/main.zip && unzip main.zip
-# cd openssh-rpms-main || echo "进入目录失败"
+# 拉取 openssh-rpms 仓库
+cd "$(mktemp -d)" || echo "进入临时目录失败"
+curl -fsSL -o main.zip https://github.com/boypt/openssh-rpms/archive/refs/heads/main.zip && unzip main.zip
+cd openssh-rpms-main || echo "进入目录失败"
 
-# # 编译 openssh
-# ./pullsrc.sh && ./compile.sh
+# 编译 openssh
+./pullsrc.sh && ./compile.sh
 
-# # 查看编译文件
-# cd "$(./compile.sh RPMDIR)" && ls
+# 查看编译文件
+cd "$(./compile.sh RPMDIR)" && ls
 
-echo "$(date)" > test
+# 拷贝编译文件至工作目录
+cp -a ./*.rpm /github/workspace
